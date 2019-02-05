76de63f0354370bab790758af6a5d82d6a96158f
johnhunsley <jphunsley@gmail.com>
2019-02-05 15:09:54 +0000

    logging

--- src/main/java/com/hunsley/async/aggregator/controller/ConsolidatedAccountController.java
+++ src/main/java/com/hunsley/async/aggregator/controller/ConsolidatedAccountController.java
@@ -3,6 +3,8 @@ package com.hunsley.async.aggregator.controller;
 
 import com.hunsley.async.aggregator.ConsolidatedAccount;
 import com.hunsley.async.aggregator.service.AccountConsolidationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +22,11 @@ public class ConsolidatedAccountController {
     @Autowired
     private AccountConsolidationService accountConsolidationService;
 
+    private Logger logger = LoggerFactory.getLogger(ConsolidatedAccountController.class);
+
     @RequestMapping(value = "accounts", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<ConsolidatedAccount> getConsolidatedAccount() throws ExecutionException, InterruptedException {
+        logger.info("Getting accounts.....");
         return new ResponseEntity<>(accountConsolidationService.getConsolidateAccounts(), HttpStatus.OK);
     }
 }
